e35b4748a1a29c3461d4434888de58f8499731ee
Anish  Ganga <anishganga@MacBook-Pro.local>
2021-09-14 20:59:44 +0530

    added django-jet admin theme

--- LMS/settings.py
+++ LMS/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,7 +52,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'LMS.urls'
 AUTH_USER_MODEL = 'accounts.Account'
-
 
 TEMPLATES = [
     {
